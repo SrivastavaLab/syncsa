acb249d0f30090f31dd3d3ca27d247ca538a7d60
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-12-02 00:55:01 -0800

    reformatting code

--- Rscripts/ScriptSYNCSA_Tutorial.R
+++ Rscripts/ScriptSYNCSA_Tutorial.R
@@ -1,13 +1,37 @@
-W <- read.table("W_14c_81spp.txt", nrows=81, header=TRUE) #read community data
-B <- read.table("B_81spp_8t.txt", nrows=81, header=TRUE) #read trait data
-E <- read.table("E_14c_1v.txt", nrows=14, header=TRUE) #read environmental data
-DistPhyl <- read.table("81spp_PhylDissim.txt", nrows=81, header=TRUE) #read phylogenetic distance matrix
+## tutorial in the SYNCSA package by Valerio Pillar
 
-organize.syncsa(W, B, DistPhyl, E)
-syncsa (W, B, DistPhyl, E, method = "pearson", dist = "euclidean", scale = TRUE, scale.envir = TRUE, permutations = 999, na.rm = FALSE, notification = TRUE) 
-syncsa (W, B, DistPhyl, E, method = "pearson", dist = "euclidean", scale = TRUE, scale.envir = FALSE, permutations = 999, na.rm = FALSE, notification = TRUE) 
+# packages ----------------------------------------------------------------
 
-optimal(W, E, B, subset.min = 1, subset.max = 8, pattern = "tcap", dist = "euclidean", method = "pearson", scale = TRUE, scale.envir = TRUE, na.rm = FALSE, notification = TRUE, progressbar = TRUE)
-optimal(W, E, B, subset.min = 1, subset.max = 8, pattern = "tdap", dist = "euclidean", method = "pearson", scale = TRUE, scale.envir = TRUE, na.rm = FALSE, notification = TRUE, progressbar = TRUE)
-optimal(W, E, B, subset.min = 1, subset.max = 8, pattern = "tcap.tdap", dist = "euclidean", method = "pearson", scale = TRUE, scale.envir = TRUE, na.rm = FALSE, notification = TRUE, progressbar = TRUE)
+library(SYNCSA)
+
+
+# data --------------------------------------------------------------------
+
+W <- read.table("data/W_14c_81spp.txt", nrows=81, header=TRUE) #read community data
+B <- read.table("data/B_81spp_8t.txt", nrows=81, header=TRUE) #read trait data
+E <- read.table("data/E_14c_1v.txt", nrows=14, header=TRUE) #read environmental data
+DistPhyl <- read.table("data/81spp_PhylDissim.txt", nrows=81, header=TRUE) #read phylogenetic distance matrix
+
+data <- organize.syncsa(W, B, DistPhyl, E)
+
+set.seed(12345)
+syncsa(comm = data$community,
+       traits = data$traits,
+       dist.spp = data$dist.spp,
+       envir = data$environmental,
+       method = "pearson", dist = "euclidean", scale = TRUE,
+       scale.envir = TRUE, permutations = 999, na.rm = FALSE, notification = TRUE) 
+
+optimal(comm = data$community,
+        traits = data$traits,
+        dist.spp = data$dist.spp,
+        subset.min = 1, subset.max = 8, pattern = "tcap", dist = "euclidean", method = "pearson", scale = TRUE, scale.envir = TRUE, na.rm = FALSE, notification = TRUE, progressbar = TRUE)
+optimal(comm = data$community,
+        traits = data$traits,
+        dist.spp = data$dist.spp,
+        subset.min = 1, subset.max = 8, pattern = "tdap", dist = "euclidean", method = "pearson", scale = TRUE, scale.envir = TRUE, na.rm = FALSE, notification = TRUE, progressbar = TRUE)
+optimal(comm = data$community,
+        traits = data$traits,
+        dist.spp = data$dist.spp,
+        subset.min = 1, subset.max = 8, pattern = "tcap.tdap", dist = "euclidean", method = "pearson", scale = TRUE, scale.envir = TRUE, na.rm = FALSE, notification = TRUE, progressbar = TRUE)
 
